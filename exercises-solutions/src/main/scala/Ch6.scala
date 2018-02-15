89d74ca2e9953dfa6728d8e63bcd3ad837fc2b78
joseph.kim <joseph.kim@workday.com>
2018-02-15 01:21:19 -0800

    Ch6 Ex1 P1-5

--- exercises-solutions/src/main/scala/Ch6.scala
+++ exercises-solutions/src/main/scala/Ch6.scala
@@ -1,9 +1,10 @@
 package swscala
 
-//import cats.syntax.functor._
 import cats.{Contravariant, Functor, derive}
 //import cats.syntax.functor._
 //import example.Filterable._
+//import org.scalacheck.ScalacheckShapeless._
+import io.chymyst.ch._
 import example.FilterableWithFilter
 
 object Ch6 {
@@ -40,13 +41,110 @@ object Ch6 {
       // For example, if you have val c = RememberAll(1, 2, 4, 1, 2, 3, 6)
       // predicate 1: x > 3
       // predicate 2: x < 3
-      // c.filter(p1).filter(p2) = ForgotMon(4, 1, 2, 3, 6).filter(p2) = ForgotTue(1, 2, 3, 6)
-      // c.filter(p1 && p2) = ForgotAll()
+      // filter(p1) ◦ filter(p2) = ForgotMon(4, 1, 2, 3, 6) ◦ filter(p2) = ForgotTue(1, 2, 3, 6)
+      // filter(p1 ◦ p2) = ForgotAll()
     }
 
-    // object Problem2 {
+    object Problem2 {
 
+      implicit val functorIndexedSeq = new Functor[IndexedSeq] {
+        override def map[A, B](fa: IndexedSeq[A])(f: A ⇒ B): IndexedSeq[B] = fa.map(f)
+      }
+
+      implicit val evenFilterableIndexedSeq = new FilterableWithFilter[IndexedSeq] {
+        def evenFilter[A](p: A => Boolean)(fa: IndexedSeq[A]): IndexedSeq[A] = {
+          if (fa.filter(t => !p(t)).length % 2 == 1) IndexedSeq() // Can we do this?
+          else fa.filter(p)
+        }
+
+        override def withFilter[A](p: A => Boolean)(fa: IndexedSeq[A]): IndexedSeq[A] =
+          evenFilter(p)(fa)
+      }
+
+      // IndexedSeq with evenFilter is not a filterable functor because it violates the conjunction law
+      // Counterexample:
+      // val s = IndexedSeq(1, 2, 3, 4, 5, 6)
+      // predicate 1: x >= 2
+      // predicate 2: x > 2
+      // filter(p1) ◦ filter(p2) = IndexedSeq() ◦ filter(p2) = IndexedSeq()
+      // filter(p1 ◦ p2) = IndexedSeq(3, 4, 5, 6)
+    }
+
+    // object Problem3 {
+    //   F[A] ≡ Int + String × A × A × A
+
+    //   // F[A] is not Not filterable since it is unclear what can be done to filter (String, A, A, A).
+    //   // If any of the A elements do not pass the boolean function, we need to return Left(i:Int) but there is nowhere to get the integer value
+
+    //   type F[A] = Either[Int, (String, A, A, A)]
+
+    //   implicit val functorF = derive.functor[F]
+
+    //   implicit val filterableF = new FilterableWithFilter[F] {
+    //     override def withFilter[A](p: A => Boolean)(fa: F[A]): F[A] = fa match {
+    //       case Left(i) => Left(i)
+    //       case Right((_, a1, a2, a3)) if p(a1) && p(a2) && p(a3) => fa
+    //       case _ => Left(i)
+    //     }
+    //   }
     // }
+
+    object Problem4 {
+      // final case class Q[A, Z](id: Long, user1: Option[(A, Z)], user2: Option[(A, Z)]) – with respect to the type parameter A
+      final case class Q[A, Z](
+        id: Long,
+        user1: Option[(A, Z)],
+        user2: Option[(A, Z)]
+      )
+
+      implicit def functorQ[Z] = new Functor[Q[?, Z]] {
+        override def map[A, B](fa: Q[A, Z])(f: A ⇒ B): Q[B, Z] = {
+          val user1New = fa.user1 match {
+            case Some((a, z)) => Some((f(a), z))
+            case _ => None
+          }
+          val user2New = fa.user2 match {
+            case Some((a, z)) => Some((f(a), z))
+            case _ => None
+          }
+          Q(fa.id, user1New, user2New)
+        }
+      }
+
+      implicit def filterableQ[Z]: FilterableWithFilter[Q[?, Z]] = new FilterableWithFilter[Q[?, Z]]() {
+        override def withFilter[A](p: A ⇒ Boolean)(fa: Q[A, Z]): Q[A, Z] = {
+          val user1New = fa.user1 match {
+            case Some((a, _)) if p(a) => fa.user1
+            case _ => None
+          }
+          val user2New = fa.user2 match {
+            case Some((a, _)) if p(a) => fa.user2
+            case _ => None
+          }
+          Q(fa.id, user1New, user2New)
+        }
+      }
+    }
+
+    object Problem5 {
+      // F[A] = MyTreeA defined recursively as F[A] ≡ 1 + A × F[A] × F[A]
+      sealed trait MyTree[A]
+      final case class MyTreeNil[A]() extends MyTree[A]
+      final case class MyTreeNode[A](v: A, l: MyTree[A], r: MyTree[A]) extends MyTree[A]
+
+      implicit val functorMyTree = derive.functor[MyTree]
+      implicit val withFilterMyTree = new FilterableWithFilter[MyTree] {
+        override def withFilter[A](p: A => Boolean)(fa: MyTree[A]): MyTree[A] = fa match {
+          case MyTreeNode(v, l, r) if p(v) => MyTreeNode(v, withFilter(p)(l), withFilter(p)(r))
+          case _ => MyTreeNil()
+        }
+      }
+    }
+
+    object Problem6 {
+
+    }
+
 
   }
 
--- exercises-solutions/src/test/scala/Ch6Spec.scala
+++ exercises-solutions/src/test/scala/Ch6Spec.scala
@@ -33,4 +33,56 @@ class Ch6Spec extends FlatSpec with FilterableLawChecking {
     //checkFilterableLawsWithFilter[Confucious, Boolean, Int]()
   }
 
+  it should "Problem 2" in {
+    import Part1.Problem2._
+
+    evenFilterableIndexedSeq.evenFilter[Int](_ > 3)(IndexedSeq(0, 1, 2, 3, 4, 5)) shouldEqual IndexedSeq(4, 5)
+    evenFilterableIndexedSeq.evenFilter[Int](_ > 3)(IndexedSeq(1, 2, 3, 4, 5)) shouldEqual IndexedSeq()
+
+    // Fails
+    // checkFilterableLawsWithFilter[IndexedSeq, Int, Boolean]()
+    // checkFilterableLawsWithFilter[IndexedSeq, String, Double]()
+
+    // Counterexample:
+    val s = IndexedSeq(1, 2, 3, 4, 5, 6)
+    def p1(x: Int): Boolean = x >= 2
+    def p2(x: Int): Boolean = x > 2
+    // def p12(x: Int): Boolean = p1(x) && p2(x)
+//     val r1 = for {
+//       x <- s
+//       if p1(x)
+// //      if p2(x)
+//     } yield x
+//     r1 shouldEqual IndexedSeq()
+
+//     // val r2 = for {
+//     //   x <- s
+//     //   if p12(x)
+//     // } yield x
+//     // r1 should not equal r2
+    // doesn't work probably because filter syntax is already implemented for IndexedSeq
+
+    val r1 = evenFilterableIndexedSeq.evenFilter(p1)(s)
+    r1 shouldEqual IndexedSeq()
+    val r2 = evenFilterableIndexedSeq.evenFilter(p2)(r1)
+    r2 shouldEqual IndexedSeq()
+    val r3 = evenFilterableIndexedSeq.evenFilter((x: Int) => p1(x) && p2(x))(s)
+    r3 shouldEqual IndexedSeq(3, 4, 5, 6)
+    r2 should not equal r3
+  }
+
+  it should "Problem 4" in {
+    import Part1.Problem4._
+
+    checkFilterableLawsWithFilter[Q[?, Boolean], Int, Boolean]()
+    checkFilterableLawsWithFilter[Q[?, Int], String, Double]()
+  }
+
+  it should "Problem 5" in {
+    import Part1.Problem5._
+
+    checkFilterableLawsWithFilter[MyTree, Int, Boolean]()
+    checkFilterableLawsWithFilter[MyTree, String, Double]()
+  }
+
 }
